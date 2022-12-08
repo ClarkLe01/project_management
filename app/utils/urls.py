594a7f203d64eab43d9b955db8889f3647dbb9ab
waflol <clark.le.goldenowl@gmail.com>
2022-12-09 02:07:08 +0700

    update

--- app/utils/urls.py
+++ app/utils/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProjectLangsView
+from .views import ProjectLangsView, ProjectLangsStatisticView
 
 
 app_name = 'utils'
 urlpatterns = [
     path('projectlangs/', ProjectLangsView.as_view({'get': 'list'}), name='projectlangs'),
+    path('projectlangstatistics/', ProjectLangsStatisticView.as_view({'get': 'list'}), name='projectlangstatistics'),
 ]
