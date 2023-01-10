d0b77a38d7f148172fa3aa9c071f0915d6f1585a
ClarkLe01 <clark.le.goldenowl@gmail.com>
2023-01-10 18:04:08 +0700

    update add comment

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -23,6 +23,9 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(notification.routing.websocket_urlpatterns)),
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(notification.routing.websocket_urlpatterns))
+        ),
+        # AuthMiddlewareStack(URLRouter(notification.routing.websocket_urlpatterns)
     }
 )
