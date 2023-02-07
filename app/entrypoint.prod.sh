1fda44c1d280d962e335683903b8ae852e76d99c
ClarkLe01 <clark.le.goldenowl@gmail.com>
2023-02-07 09:26:44 +0700

    update

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -12,6 +12,12 @@ then
 fi
 python manage.py flush --no-input
 python manage.py migrate
+python manage.py loaddata fixtures/langprogramming.json --app utils.ProgrammingLanguage
+python manage.py loaddata fixtures/currencies.json --app utils.Currency
+python manage.py loaddata fixtures/users.json --app user.User
+python manage.py loaddata fixtures/projects.json --app project.Project
+python manage.py loaddata fixtures/files.json --app project.File
+python manage.py loaddata fixtures/tasks.json --app project.Task
 daphne -b 0.0.0.0 -p 8000 app.asgi:application
 exec "$@"
 
